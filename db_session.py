3dadefad766b8ad3624aecde4f72f1cb03c00dc3
Chiff001 <i.d.sidorov@yandex.ru>
2021-04-25 18:22:35 +0300

    2 коммит

--- db_session.py
+++ db_session.py
@@ -3,6 +3,8 @@ import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 import sqlalchemy.ext.declarative as dec
 
+# Пытался добавить в проект базу данных, чтобы в ней хранились оценки сайта. Но SQLstudio перестал работать.
+# Не открывал базы данных и не создавал их. Я переустанавливал его но не помогло. Я создал бд, но не могу открыть и посмотреть ее
 Base = dec.declarative_base()
 engine = sqlalchemy.create_engine('sqlite:///review.db')
 Base.metadata.create_all(engine)
@@ -13,5 +15,4 @@ class User(Base):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     number = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
